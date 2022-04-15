708ccc24d35a0ef76f7a34122ae0cda90219d4f7
Adam Achebe <mister363636@gmail.com>
2022-04-14 23:19:06 -0500

    Modified imports

--- src/agent.ts
+++ src/agent.ts
@@ -1,18 +1,12 @@
 import {
-  BlockEvent,
   Finding,
-  HandleBlock,
   HandleTransaction,
   TransactionEvent,
   FindingSeverity,
   FindingType,
 } from "forta-agent";
 
-const SWAP_EVENT_1 =
-  "event Swap( address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to )";
-
-const SWAP_EVENT2 =
-  "event Swap( address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick )";
+import { SWAP_EVENT_1, SWAP_EVENT2 } from "./constants";
 
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SWAP_EVENT_1 =
+  "event Swap( address indexed sender, uint amount0In, uint amount1In, uint amount0Out, uint amount1Out, address indexed to )";
+
+export const SWAP_EVENT2 =
+  "event Swap( address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick )";
