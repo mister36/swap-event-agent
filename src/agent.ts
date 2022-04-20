6a497721ef42368b9f047360dce53b5641ab65a0
Adam Achebe <mister363636@gmail.com>
2022-04-19 21:04:39 -0500

    Improved performance

--- src/agent.ts
+++ src/agent.ts
@@ -46,76 +46,76 @@ const handleTransaction: HandleTransaction = async (
   const swaps_1 = txEvent.filterLog(SWAP_EVENT_1);
   const swaps_2 = txEvent.filterLog(SWAP_EVENT2);
 
+  // console.log(swaps_1);
+  // console.log(swaps_2);
+
   if (swaps_1.length > 0 || swaps_2.length > 0) {
     let token0, token1, fee;
+
+    const swapLogs = swaps_1.length === 0 ? swaps_2 : swaps_1;
+    const addresses = swapLogs.map((log) => log.address);
+    const swapVersion = swapLogs[0].args.sqrtPriceX96 ? 3 : 2; // version 2 "swap() has no sqrtPriceX96 arg"
+
     const provider = new ethers.providers.JsonRpcProvider(getJsonRpcUrl());
-    for (const address of Object.keys(txEvent.addresses)) {
-      if ((await provider.getCode(address)) === "0x") {
-        // if the address is an eoa
-        continue;
-      } else {
-        // creates pool contracts from V2 and V3 of Uniswap
-        const v2PairContract = new ethers.Contract(
-          address,
-          IUniswapV2PairABI,
-          provider
-        );
-
-        const v3PoolContract = new ethers.Contract(
-          address,
-          IUniswapV3PoolABI,
-          provider
-        );
-
-        /* Checks whether potential pool address in transaction equals the return 
+    for (const address of addresses) {
+      /* Checks whether potential pool address in transaction equals the return 
          of the factory "getPool/getPair" function with the token
          params coming from poolContrant.token()
 
          Ensures that swaps don't originate from Uniswap fork
         */
 
+      if (swapVersion === 3) {
+        const v3PoolContract = new ethers.Contract(
+          address,
+          IUniswapV3PoolABI,
+          provider
+        );
+
+        [token0, token1, fee] = await Promise.all([
+          v3PoolContract.token0(),
+          v3PoolContract.token1(),
+          v3PoolContract.fee(),
+        ]);
+
+        const v3FactoryContract = new ethers.Contract(
+          V3_FACTORY,
+          V3_FACTORY_ABI,
+          provider
+        );
+
+        const pool = await v3FactoryContract.getPool(token0, token1, fee);
+
+        if (pool.toLowerCase() === address) {
+          findings.push(findingMaker(pool, hash, token0, token1));
+        }
+      } else {
+        const v2PairContract = new ethers.Contract(
+          address,
+          IUniswapV2PairABI,
+          provider
+        );
+
         try {
-          // Tries V3 pool contract first
-          [token0, token1, fee] = await Promise.all([
-            v3PoolContract.token0(),
-            v3PoolContract.token1(),
-            v3PoolContract.fee(),
+          [token0, token1] = await Promise.all([
+            v2PairContract.token0(),
+            v2PairContract.token1(),
           ]);
 
-          const v3FactoryContract = new ethers.Contract(
-            V3_FACTORY,
-            V3_FACTORY_ABI,
+          const v2FactoryContract = new ethers.Contract(
+            V2_FACTORY,
+            V2_FACTORY_ABI,
             provider
           );
 
-          const pool = await v3FactoryContract.getPool(token0, token1, fee);
+          const pool = await v2FactoryContract.getPair(token0, token1);
 
           if (pool.toLowerCase() === address) {
             findings.push(findingMaker(pool, hash, token0, token1));
           }
         } catch (error) {
-          // Tries V2 pool contract next
-          try {
-            [token0, token1] = await Promise.all([
-              v2PairContract.token0(),
-              v2PairContract.token1(),
-            ]);
-
-            const v2FactoryContract = new ethers.Contract(
-              V2_FACTORY,
-              V2_FACTORY_ABI,
-              provider
-            );
-
-            const pool = await v2FactoryContract.getPair(token0, token1);
-
-            if (pool.toLowerCase() === address) {
-              findings.push(findingMaker(pool, hash, token0, token1));
-            }
-          } catch (error) {
-            // In case address isn't a pool contract
-            continue;
-          }
+          // In case address isn't a pool contract
+          continue;
         }
       }
     }
